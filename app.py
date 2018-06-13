3b8170d9450ceba682901463e42aa95e0b9f3966
Neil Jones <neil.j.neil@gmail.com>
2018-06-13 02:23:16 -0700

    Testing

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "TEST"
+def index():
+    return "<h1>TEST<h1>"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
