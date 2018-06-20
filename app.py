c4b8380085c5de9a7ab638cffe0e98314fb06aa7
Neil Jones <neil.j.neil@gmail.com>
2018-06-20 16:33:37 -0700

    Saving credentials in enviornment variables

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
+import os
+
+import psycopg2
+
+host = os.getenv('HOST')
+dbname = os.getenv('dbname')
+user = os.getenv('user')
+password = os.getenv('password')
+
+
+conn_string = "host="+host+" dbname="+dbname+" user="+user+" password="+password
+conn = psycopg2.connect(conn_string)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM public."Customers"')
+    rows = cur.fetchall()
+    print(rows)
+
     return "<h1>TEST<h1>"
 
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
