2d841b04cf5ac8db4fa767cb9240f8d1776fadd9
Neil Jones <neil.j.neil@gmail.com>
2018-06-21 00:13:23 -0700

    test

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, abort, request
 from config import db_context
+import json
 
 app = Flask(__name__)
 
-# Index route.
+# Homepage route.
 # Displays API documentation by returning a static html.
 @app.route('/')
 def index():
-    return current_app.send_static_file('index.html')
+    return current_app.send_static_file('apiary.apib')
+
+@app.route('/read', methods=['GET'])
+def read():
+    if not request.json:
+        abort(400)
+    print (request.json)
+    return json.dumps(request.json)
